46ede5cf7b1dc5ca2c36d0519587785cac280a37
André Werlang <beppe85@gmail.com>
2020-03-28 19:20:39 -0300

    auto-update.sh: improvements * improve robutsness * output packages which depends on hot-fixes

--- bin/auto-update.sh
+++ bin/auto-update.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 
+# Repositories
+#
+# | Alias       | Name                       | Required |
+# +-------------+----------------------------+----------+
+# | repo-oss    | openSUSE-Tumbleweed-Oss    | *        |
+# | repo-update | openSUSE-Tumbleweed-Update | *        |
+# | code        | Visual Studio Code         |          |
+# | NVIDIA      | NVIDIA                     |          |
+
 COLOR1='\033[0;33m'
 COLOR2='\033[0;34m'
 NC='\033[0m'
 
-tmpfile=$(mktemp /tmp/auto_update.XXXXXX)
+if zypper --color repos code > /dev/null; then
+    printf "${COLOR1}> Checking Visual Studio Code${NC}\n\n"
 
-printf "${COLOR1}> Checking Visual Studio Code${NC}\n\n"
-sudo zypper refresh --repo code
-sudo zypper --non-interactive --color dup --details --auto-agree-with-licenses --from code | tee $tmpfile
-if grep --extended-regexp "Installing: .*done\]" $tmpfile; then
-    printf "\n${COLOR2}>> New Visual Studio Code installed.${NC}\n"
-    notify-send -a "Visual Studio Code" -t 5000 "New VS Code installed"
-else
-    printf "\n${COLOR2}>> Done.${NC}\n"
+    sudo zypper refresh --repo code
+    if zypper list-updates --repo code | grep "^v "; then
+        sudo zypper --non-interactive --color dup --details --auto-agree-with-licenses --from code
+        printf "\n${COLOR2}>> New Visual Studio Code installed.${NC}\n"
+        notify-send -a "Visual Studio Code" -t 5000 "New VS Code installed"
+    else
+        printf "\n${COLOR2}>> Done.${NC}\n"
+    fi
+
+    echo ""
 fi
-
-echo ""
 
 SKIP_SYSTEM_REASON=""
 
-printf "${COLOR1}> Checking NVIDIA${NC}\n\n"
 if zypper --color repos NVIDIA > /dev/null; then
+    printf "${COLOR1}> Checking NVIDIA${NC}\n\n"
+
     sudo zypper refresh --repo NVIDIA
-    sudo zypper --non-interactive --color dup --dry-run --details --auto-agree-with-licenses --from NVIDIA | tee $tmpfile
-    if grep --extended-regexp "Nothing to do." $tmpfile > /dev/null; then
-        SKIP_SYSTEM_REASON="Waiting for NVIDIA"
-        printf "\n${COLOR2}>> No updates to NVIDIA found.${NC}\n"
-    else
+    if zypper list-updates --repo NVIDIA | grep "^v "; then
         sudo zypper refresh
         sudo zypper --non-interactive --color dup --download-only --details --auto-agree-with-licenses
 
@@ -35,24 +42,26 @@ if zypper --color repos NVIDIA > /dev/null; then
         printf "${COLOR2}>> New NVIDIA driver available.${NC}\n"
         printf "${COLOR2}>> It's a good time to update the system.${NC}\n"
         notify-send -a "System update" -t 5000 "New NVIDIA driver available" "It's a good time to update the system ;)"
+    else
+        SKIP_SYSTEM_REASON="Waiting for NVIDIA"
+        printf "\n${COLOR2}>> No updates to NVIDIA found.${NC}\n"
     fi
-fi
 
-echo ""
+    echo ""
+fi
 
 printf "${COLOR1}> Checking System${NC}\n\n"
 if [ -z "$SKIP_SYSTEM_REASON" ]; then
     sudo zypper refresh
-    sudo zypper --non-interactive --color dup --dry-run --details --auto-agree-with-licenses | tee $tmpfile
-    if grep --extended-regexp "Nothing to do." $tmpfile > /dev/null; then
-        printf "\n${COLOR2}>> No updates found. Hooray!${NC}\n"
-    else
+    if zypper list-updates | grep "^v "; then
         sudo zypper --non-interactive --color dup --download-only --details --auto-agree-with-licenses
 
         echo ""
         printf "${COLOR2}>> New system updates available.${NC}\n"
         printf "${COLOR2}>> It's a good time to update the system.${NC}\n"
         notify-send -a "System update" -t 5000 "New system updates available" "It's a good time to update the system ;)"
+    else
+        printf "\n${COLOR2}>> No updates found. Hooray!${NC}\n"
     fi
 else
     printf "${COLOR2}>> Skipping System: $SKIP_SYSTEM_REASON.${NC}\n"
@@ -62,9 +71,16 @@ echo ""
 
 printf "${COLOR1}> Checking hot-fixes${NC}\n\n"
 sudo zypper refresh --repo repo-update
-if zypper search --installed-only --repo=repo-update | awk -F \| '{ print $2 }' | sort | uniq | sed 's/^ *//;s/ *$//' | grep -v -e "^Name$" -e "^$"; then
-    printf ">> Found hot-fixes for installed packages.\n"
-    notify-send -a "System update" -t 5000 "Found hot-fixes" "It's a good time to update the system ;)"
+pkglist=$(zypper list-updates | grep "openSUSE-Tumbleweed-Update" | awk -F \| '{ print $3 }')
+if [ -n "$pkglist" ]; then
+    pkgtree=""
+    while IFS= read -r pkg ; do 
+        pkgtree+="*$pkg\n"
+        deps=$(zypper search --installed-only --requires-pkg $pkg | grep "^i" | awk -F \| '{ print "  └─" $2 }' | grep -v " lib")
+        [ -n "$deps" ] && pkgtree+="$deps\n"
+    done <<< "$pkglist"
+    printf ">> Found hot-fixes for installed packages.\n$pkgtree\n"
+    notify-send -a "System update" -t 5000 "Found hot-fixes" "Packages: \n$pkgtree"
 else
     printf "\n${COLOR2}>> No urgent updates found.${NC}\n"
 fi
