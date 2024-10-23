dd0115462ce4cb6cdea9f5d774500663d08af9db
André Werlang <beppe85@gmail.com>
2024-10-23 13:56:24 -0300

    fish: add abbreviation for ping

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,5 @@ if status is-interactive
     abbr --add clip fish_clipboard_copy
     abbr --add isascii iconv -f ascii -t ascii -o /dev/null
     abbr --add isutf8 iconv -f utf-8 -t utf-8 -o /dev/null
+    abbr --add ping ping -c 4
 end
